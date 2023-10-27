2e2a827bf9cd42f3fbc147f99503ba64653257a3
Naufal Taufik Alhakim <naufaltaufik04@gmail.com>
2023-10-27 13:14:27 +0700

    initial

---
+++ api-service/src/test/java/tech/test/apiservice/ApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.test.apiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ konsumen-service/src/main/java/tech/test/konsumenservice/KonsumenServiceApplication.java
@@ -0,0 +1,13 @@
+package tech.test.konsumenservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KonsumenServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KonsumenServiceApplication.class, args);
+	}
+
+}
